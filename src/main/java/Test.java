54a6c4e9239064b1ec63d9a924f368a30186d9a9
admin <2452555049@qq.com>
2020-08-20 10:11:43 +0800

    Test 1.0 提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,5 +5,13 @@ public class Test {
         System.out.println("一个张三");
         System.out.println("一个张三");
         System.out.println("一个张三");
+
+        System.out.println("第二个张三");
+
+
+    }
+    public void test(){
+        int a = 10;
     }
 }
+
