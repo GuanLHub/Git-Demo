9c5a5f305fcc428966caca07d423371d833b730b
admin <2452555049@qq.com>
2020-08-20 09:57:16 +0800

    Test 1.0 提交

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("一个张三");
+        System.out.println("一个张三");
+        System.out.println("一个张三");
+        System.out.println("一个张三");
+    }
+}
